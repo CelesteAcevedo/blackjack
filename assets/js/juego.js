3684fbbe6a052fba87151847f07cb9c5499e133d
Celeste Acevedo <celestesukkie@gmail.com>
2023-08-23 23:07:54 -0500

    [Add] Función pedir carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ["C", "D", "H", "S"];
 const especiales = ["A", "J", "Q", "K"];
 
+//Esta función crea un nuevo deck o nueva baraja
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let tipo of tipos) deck.push(i + tipo);
@@ -18,10 +19,24 @@ const crearDeck = () => {
       deck.push(esp + tipo);
     }
   }
-
-  console.log(deck);
+  //console.log(deck);
   deck = _.shuffle(deck);
   console.log(deck);
+  return deck;
 };
 
 crearDeck();
+
+//Esta función me permite tomar un carta
+const pedirCarta = () => {
+  if (deck.length === 0) {
+    throw "Nohay cartas en el deck";
+  }
+
+  const carta = deck.shift();
+  console.log(carta);
+  return carta;
+};
+
+//pedirCarta();
+const valorCarta = () => {};
